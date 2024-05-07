ea106884a35f7f60c5b12524a4feab78974b0d67
Dennis Rodriguez <dennisrodriguezx@gmail.com>
2024-05-07 19:21:57 -0400

    Init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Juegos Arquitectura App (juegos-arquitectura)
+
+Proyecto para manejar los roles de partidos y diferente informacion para los participantes.
+
+## 1. Install nvm
+
+Install nvm (node version manager) and use 18 version of node
+
+## 2. Install dependecies
+
+```bash
+yarn
+# or
+npm install
+```
+
+## 3. Dev mode runing
+
+```bash
+quasar dev
+# or
+yarn quasar dev
+```
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page>
+    <div class="bg-indigo-10 text-white flex flex-center">
+      <h4 class="q-ma-none">155 : 22 : 44 :40</h4>
+    </div>
+    <div class="q-px-sm q-gutter-md">
+      <q-btn unelevated class="full-width" text-color="black" color="yellow-8" label="Fixture y Resultados" />
+      <q-btn unelevated class="full-width" text-color="black" color="yellow-8" label="Juegos en curso" />
+      <q-btn unelevated class="full-width" text-color="black" color="yellow-8" label="Hospedaje" />
+      <q-btn unelevated class="full-width" text-color="black" color="yellow-8" label="Comida" />
+      <q-btn unelevated class="full-width" text-color="black" color="yellow-8" label="Lugares Turisticos" />
+      <q-btn unelevated class="full-width" text-color="black" color="yellow-8" label="Eventos Especiales" />
+      <q-btn unelevated class="full-width" text-color="black" color="yellow-8" label="Ganadores" />
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
