16e898a1c2b484e1f23d954905a960568432af91
Dennis Rodriguez <dennisrodriguezx@gmail.com>
2024-05-08 17:44:57 -0400

    FEAT: Fixture, juegos page added & examples

---
+++ src/pages/HospedajePage.vue
@@ -0,0 +1,10 @@
+<template>
+    <h4>Hospedaje</h4>
+    <li>lugar 1</li>
+    <li>lugar 1</li>
+    <li>lugar 1</li>
+    <li>lugar 1</li>
+    <div>
+        AQUI VIENE EL MAPA DE GOOGLE
+    </div>
+</template>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,16 +1,30 @@
 <template>
   <q-page>
-    <div class="bg-indigo-10 text-white flex flex-center">
-      <h4 class="q-ma-none">155 : 22 : 44 :40</h4>
+    <div class="bg-indigo-10 text-white row justify-center">
+      <p class="q-ma-none">DIAS / HORAS / MINUTOS / SEGUNDOS</p>
+      <h4 class="q-ma-none">155 : 22 : 44 : 40</h4>
     </div>
-    <div class="q-px-sm q-gutter-md">
-      <q-btn unelevated class="full-width" text-color="black" color="yellow-8" label="Fixture y Resultados" />
-      <q-btn unelevated class="full-width" text-color="black" color="yellow-8" label="Juegos en curso" />
-      <q-btn unelevated class="full-width" text-color="black" color="yellow-8" label="Hospedaje" />
-      <q-btn unelevated class="full-width" text-color="black" color="yellow-8" label="Comida" />
-      <q-btn unelevated class="full-width" text-color="black" color="yellow-8" label="Lugares Turisticos" />
-      <q-btn unelevated class="full-width" text-color="black" color="yellow-8" label="Eventos Especiales" />
-      <q-btn unelevated class="full-width" text-color="black" color="yellow-8" label="Ganadores" />
+    <div class="bg-indigo-10 q-px-sm">
+      <q-btn unelevated class="full-width q-mb-sm" align="left" text-color="black" color="yellow-8" icon="event"
+        label="Fixture y Resultados">
+        <router-link exact to="/fixture" class="absolute full-width full-height"></router-link>
+      </q-btn>
+      <q-btn unelevated class="full-width q-mb-sm" align="left" text-color="black" color="yellow-8"
+        icon="sports_handball" label="Juegos en curso">
+        <router-link exact to="/juegoscurso" class="absolute full-width full-height"></router-link>
+      </q-btn>
+      <q-btn unelevated class="full-width q-mb-sm" align="left" text-color="black" color="yellow-8" icon="hotel"
+        label="Hospedaje">
+        <router-link exact to="/hospedaje" class="absolute full-width full-height"></router-link>
+      </q-btn>
+      <q-btn unelevated class="full-width q-mb-sm" align="left" text-color="black" color="yellow-8"
+        icon="restaurant_menu" label="Comida" />
+      <q-btn unelevated class="full-width q-mb-sm" align="left" text-color="black" color="yellow-8" icon="location_on"
+        label="Lugares Turisticos" />
+      <q-btn unelevated class="full-width q-mb-sm" align="left" text-color="black" color="yellow-8" icon="nightlife"
+        label="Eventos Especiales" />
+      <q-btn unelevated class="full-width q-mb-sm" align="left" text-color="black" color="yellow-8" icon="emoji_events"
+        label="Ganadores" />
     </div>
   </q-page>
 </template>
